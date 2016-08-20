1ae08766f8cae4b39fafd2039a712680253106e1
Vladimir Statsenko <vovchiksta@gmail.com>
2016-08-20 15:37:02 +0300

    ch1.4

--- ch1.4/lissajous.go
+++ ch1.4/lissajous.go
@@ -11,7 +11,11 @@ import (
 	"os"
 )
 
-var palette = []color.Color{color.White, color.Black}
+var palette = []color.Color{color.RGBA{0x00, 0x00, 0x00, 0xFF},
+	color.RGBA{0x00, 0xFF, 0x00, 0xFF},
+	color.RGBA{0xFF, 0xFF, 0x00, 0xFF},
+	color.RGBA{0x00, 0xFF, 0xFF, 0xFF},
+	color.RGBA{0xFF, 0x00, 0xFF, 0xFF}}
 
 const (
 	whiteIndex = 0 // first color in palette
@@ -39,7 +43,7 @@ func lissajous(out io.Writer) {
 			x := math.Sin(t)
 			y := math.Sin(t*freq + phase)
 			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5),
-				blackIndex)
+				uint8(i%len(palette[1:])+1))
 		}
 		phase += 0.1
 		anim.Delay = append(anim.Delay, delay)
